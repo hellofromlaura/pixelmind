b02af565900b7927382153df8146c11f41e29da6
Laura <laurawolfart@gmail.com>
2023-08-28 16:10:40 -0400

    add build path

--- server/index.js
+++ server/index.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')))
+}
+
 app.use('/', routes);
 
 app.listen(PORT, () => {
