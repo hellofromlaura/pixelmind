83bd50dee7960883f15b24e905595bb6a9dd52d2
Laura <laurawolfart@gmail.com>
2023-08-28 17:42:03 -0400

    adding path

--- server/index.js
+++ server/index.js
@@ -2,12 +2,17 @@ import express from 'express';
 import { routes } from './routes.js'
 import cors from 'cors';
 import path from 'path'
+import { fileURLToPath } from 'url';
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
 
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')))
