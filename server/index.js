65fb8798e31cb3a341914dd1967d9937fd453681
Laura <laurawolfart@gmail.com>
2023-08-28 17:56:08 -0400

    adding path

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')))
+    app.use(express.static(path.join(__dirname, '../../client/build')))
 }
 
 app.use('/', routes);
